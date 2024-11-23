96c941e3804c35701900b7963ab7e66dc61f7b6f
andrewmanuelt <andrewmanuelt@gmail.com>
2024-11-23 14:56:57 +0700

    update: DPR retriever

--- main.py
+++ main.py
@@ -2,42 +2,31 @@ import pandas as pd
 
 from utility.db import VectorDB
 from utility.document import DocumentLoader
+from utility.general import check_device
 
 from hyperparameter.tuning import RetrieverTuning
 
 def documents():
-    loader = DocumentLoader(dataset_path='./dataset/dummy.json')
+    loader = DocumentLoader(dataset_path='./dataset/single/single_train.json')
     return loader.documents()
     
 def main(): 
-    # db = VectorDB(
-    #     collection_name='dummy', 
-    #     presistent_path='./database/dummy'
-    # )
+    db = VectorDB(
+        collection_name='single', 
+        presistent_path='./database/single'
+    )
     
-    # db.add_document(
-    #     documents=documents(),
-    #     chunk_size=100,
-    #     chunk_overlap=10
-    # )
+    db.add_document(
+        documents=documents(),
+    )
     
-    # query = "Di mana terletak di provinsi trento ?"
     # result = db.get_document(
-    #     query=query,
-    #     top_k=5
+    #     query="Di mana terletak di provinsi trento ?",
+    #     top_k=5,
     # )
+    
     # print(result)
     
-    # tuner = RetrieverTuning(
-    #     collection_name='dummy', 
-    #     presistent_path='./database/dummy', 
-    #     test_path='./dataset/dummy_test.json',
-    #     train_path='./dataset/dummy.json'
-    # )
-    # tuner.tuning()
-    
-    
-    
-    
 if __name__ == '__main__':
+    check_device()
     main()
--- utility/db.py
+++ utility/db.py
@@ -7,13 +7,17 @@ from embedding.dense import DPRetriever
 from utility.general import similarity
 from utility.general import splitter
 
-class VectorDB(DPRetriever):
+class VectorDB():
     def __init__(self, collection_name, presistent_path) -> None:
         self.collection_name = collection_name
         self.presistent_path = presistent_path
+        
+        self._init_param()
+
+    def _init_param(self):
         self.client = self._init_client()
         self.collection = self._init_collection()
-    
+
     def _init_client(self):
         return chromadb.PersistentClient(
             path=self.presistent_path
@@ -21,35 +25,47 @@ class VectorDB(DPRetriever):
         
     def _init_collection(self):
         return self.client.get_or_create_collection(self.collection_name)
-    
-    def add_document(self, documents, chunk_size : int, chunk_overlap : int):
+        
+    def add_document(self, documents, chunk_size : int = None, chunk_overlap : int = None):
+        retriever = DPRetriever()
+        
         for doc in tqdm(documents):
             if chunk_size is not None and chunk_overlap is not None:
                 try:
-                    text_split = splitter(
+                    sentence_split = splitter(
                         text=doc['context'],
                         chunk_size=chunk_size,
                         chunk_overlap=chunk_overlap
                     )
                     
-                    for text in text_split:
-                        self._document_processing(text)
+                    for sentence in sentence_split:
+                        self._document_processing(sentence, retriever)
                 except Exception as e:
                     print(e)
             else:
-                self._document_processing(text)
+                self._document_processing(doc['context'], retriever)
             
-    def _document_processing(self, text):
-        doc_embedding = self.context_to_embedding(text)
+    def _document_processing(self, sentence, retriever):
+        doc_embedding = retriever.do_context(
+            sentence=sentence,
+            model=retriever.context_model(),
+            tokenizer=retriever.context_tokenizer()
+        )
         
         self.collection.add(
                 ids=[str(uuid.uuid4().hex)],
-                documents=[text],
-                embeddings=[doc_embedding]
+                documents=[sentence],
+                embeddings=doc_embedding
             )
         
     def get_document(self, query : str, top_k : int) -> list:
-        query_embeddings = self.question_to_embedding(query=query)
+        retriever = DPRetriever()
+        
+        query_embeddings = retriever.do_context(
+            sentence=query,
+            model=retriever.context_model(),
+            tokenizer=retriever.context_tokenizer()
+        )
         
         results = self.collection.query(
             query_embeddings=query_embeddings, 
@@ -60,14 +76,18 @@ class VectorDB(DPRetriever):
         document_result = []
         
         for index, document in enumerate(results["documents"][0]):
-            candidate_embedding = self.context_to_embedding(text=document)    
-
+            candidate_embedding = retriever.do_question(
+                model=retriever.question_model(),
+                tokenizer=retriever.question_tokenizer(),
+                sentence=document
+            )    
+            
             rank = index + 1
+            
             cs = similarity(query=query_embeddings, candidate=candidate_embedding)
             distance_per_index = distance[index]
             
             docset = (rank, distance_per_index, cs, document)
-            
             document_result.append(docset)
             
         return document_result
